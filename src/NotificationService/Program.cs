a5440e57742994514a3d3023f3e6bae65f0a808f
kevin <kevin@stepincto.com>
2024-07-26 23:15:14 +1000

    add signalR services

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,27 @@
+using MassTransit;
+using NotificationService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x => {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("notification", false));
+
+    // using rabbitMq
+    x.UsingRabbitMq((context, cfg) => {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Username(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notification");
+
+app.Run();
